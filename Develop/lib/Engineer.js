934fab0ad04b90af774b5a7b6be2951a8ad7b925
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-05-07 22:41:07 -0500

    Added the basic files and the installed needed packages

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,24 @@
+class Engineer {
+    constructor (name, id, email, github) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.github = github;
+    }
+
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getGithub() {
+        return this.github;
+    }
+    getRole() {
+        return 'Engineer';
+    }
+}
---
+++ Develop/lib/Manager.js
@@ -0,0 +1,24 @@
+class Manager {
+    constructor (name, id, email, officenumber) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.officenumber = officenumber;
+    }
+
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getOfficenumber() {
+        return this.officenumber;
+    }
+    getRole() {
+        return 'Engineer';
+    }
+}
