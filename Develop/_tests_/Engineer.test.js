a3231fd3512d1ea232d3919dfa3cf21096bfe9ff
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-05-07 23:58:08 -0500

    Cleaned up the test files a bit

--- Develop/_tests_/Engineer.test.js
+++ Develop/_tests_/Engineer.test.js
@@ -1,13 +1,11 @@
 const Engineer = require ('../lib/Engineer');
 
-describe ("Engineer", () => {
-    test("This should return engineer's name, id, email, github, and role", () => {
-        let engineerTest = new Engineer ("Paul Wall", "02", "test2@email.com", "PaulWall281");
+test("This should return the engineer's name, id, email, github, and role", () => {
+    let engineerTest = new Engineer ("Paul Wall", "02", "test2@email.com", "PaulWall281");
 
-        expect(engineerTest.getRole()).toEqual("Engineer");
-        expect(engineerTest.getName()).toEqual("Paul Wall");
-        expect(engineerTest.getId()).toEqual("02");
-        expect(engineerTest.getEmail()).toEqual("test2@email.com");
-        expect(engineerTest.getGithub()).toEqual("PaulWall281");
-    })
+    expect(engineerTest.getRole()).toEqual("Engineer");
+    expect(engineerTest.getName()).toEqual("Paul Wall");
+    expect(engineerTest.getId()).toEqual("02");
+    expect(engineerTest.getEmail()).toEqual("test2@email.com");
+    expect(engineerTest.getGithub()).toEqual("PaulWall281");
 });
--- Develop/_tests_/Manager.test.js
+++ Develop/_tests_/Manager.test.js
@@ -1,13 +1,11 @@
 const Manager = require ('../lib/Manager');
 
-describe ("Manager", () => {
-    test("This should return Manager's name, id, email, github, and role", () => {
-        let ManagerTest = new Manager ("James Prince", "01", "test1@email.com", "2818308004");
+test("This should return the manager's name, id, email, github, and role", () => {
+    let ManagerTest = new Manager ("James Prince", "01", "test1@email.com", "2818308004");
 
-        expect(ManagerTest.getRole()).toEqual("Manager");
-        expect(ManagerTest.getName()).toEqual("James Prince");
-        expect(ManagerTest.getId()).toEqual("01");
-        expect(ManagerTest.getEmail()).toEqual("test1@email.com");
-        expect(ManagerTest.getOfficenumber()).toEqual("2818308004");
-    })
+    expect(ManagerTest.getRole()).toEqual("Manager");
+    expect(ManagerTest.getName()).toEqual("James Prince");
+    expect(ManagerTest.getId()).toEqual("01");
+    expect(ManagerTest.getEmail()).toEqual("test1@email.com");
+    expect(ManagerTest.getOfficenumber()).toEqual("2818308004");
 });
